88682e62ddc85edd780d4594c3997cf9ecec351a
Adailton Paz <adailtonpaz@pazimports.onmicrosoft.com>
2025-02-07 14:47:49 -0300

    CRUD API WITH ANGULAR AND JAVA SPRING BOOT

--- src/components/API/post-api/post-api.component.ts
+++ src/components/API/post-api/post-api.component.ts
@@ -1,18 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-post-api',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './post-api.component.html',
   styleUrl: './post-api.component.css'
 })
 export class PostApiComponent {
 
+    ngOnInit(){
+      this.getAllEstudantes();
+    }
    
     estudanteList:any [] = [];
     estudanteObj:any = {
+    "id":"",
     "nome": "",
     "email": "",
     "categoriaEstudantil": ""
@@ -25,25 +31,77 @@ export class PostApiComponent {
     getAllEstudantes(){
       this.http.get("http://localhost:8181/api/estudantes/listaestudante").subscribe((res:any) => {
         this.estudanteList = Array.isArray(res) ? res : [];
-        debugger;
+        //debugger;
       })
     }
 
     onSaveEstudantes(){
-      this.http.post("http://localhost:8181/api/estudantes/salvarestudante", this.estudanteObj).subscribe((res:any) => {
-        debugger;
-        if(res.status === 200 || res.code ===200){
-          alert("Estudante criado com sucesso");
-          this.resetForm();
-        }else{
-          alert(res.message)
-        }
-      })
-      
+      this.estudanteObj.id = "";
+      this.http.post("http://localhost:8181/api/estudantes/salvarestudante", this.estudanteObj, {observe: 'response'})
+      .subscribe({
+          next: (res: any) => {
+            if(res.status === 200 || res.status === 201){
+              //debugger;
+              alert("Estudante salvo com sucesso");
+              this.getAllEstudantes();
+              this.resetForm();
+            }else{
+              alert(res.body?.message || "Erro inesperado");
+            }
+          },
+          error: (err) => {
+            alert(err.error?.message || "Erro ao salvar");
+          }
+          });
+         }  
+
+         onEdit(data: any){
+          this.estudanteObj = data;
+          
+         }
+
+         
+
+updateEstudante() {
+    this.http.put(`http://localhost:8181/api/estudantes/alterar/${this.estudanteObj.id}`, this.estudanteObj, {observe: 'response'})
+    .subscribe({
+    next:  (res: any) => {
+      console.log(res);
+      if(res.status == 200 || res.status == 201){
+      alert("Estudante alterado com sucesso");
+      this.resetForm();
+      }else{
+        alert("Falha ao alterar");
+      }
+    }
+    });
     }
 
+    excluir(id:number){
+      this.http.delete(`http://localhost:8181/api/estudantes/excluir/${id}`, {observe: 'response'})
+      .subscribe({
+          next: (res:any) => {
+            if(res.status === 200 || res.status === 204){
+              alert("Estudante excluído com sucesso");
+              this.getAllEstudantes();
+            }else{
+              alert("Error ao excluir estudante")
+            }
+
+          },
+          error: (err) => {
+            if (err.status === 404) {
+              alert("Estudante não encontrado");
+            } else {
+              alert("Erro ao excluir estudante");
+            }
+            console.error("Erro ao excluir estudante:", err);
+          }
+      });
+    }
     resetForm(){
       this.estudanteObj = {
+        "id": "",
         "nome": "",
         "email": "",
         "categoriaEstudantil": ""
